0403ddf6aa4f3cc365c69c391ab4d89ec184c22b
Eric Santos <ericsantosbr2@gmail.com>
2024-09-26 02:51:43 -0300

    Student view improvements

--- src/components/ClassPageComponents/ClassStudentsContainer/ClassStudentsContainer.js
+++ src/components/ClassPageComponents/ClassStudentsContainer/ClassStudentsContainer.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import StudentCard from "../../studentComponents/StudentCard/StudentCard";
+import { Container, Row } from "reactstrap";
 
 export default function ClassStudentsContainer(data) {
     const [studentsRenderComponent, setStudentsRenderComponent] = useState([]);
-
+    
     useEffect(() => {
         for (let student of data.StudentsInfo) {
             setStudentsRenderComponent(studentsRenderComponent => [...studentsRenderComponent, <StudentCard StudentData={student} />])
@@ -11,6 +12,8 @@ export default function ClassStudentsContainer(data) {
     }, [data]);
 
     return (
-        <>{studentsRenderComponent}</>
+        <Container>
+            <Row>{studentsRenderComponent}</Row>
+        </Container>
     )
 }
